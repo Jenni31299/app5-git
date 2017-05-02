b6d1481433c5782be0d74687e9c3210ed52b794d
Jenni31299 <nifer@gmail.comhernandezromerojennifer@gmail.com>
2017-05-02 12:54:56 -0500

    subida2

--- js/acciones.js
+++ js/acciones.js
@@ -2,28 +2,14 @@
 
 $(document).ready(function(e) {
 	document.addEventListener("deviceready",function(){
-		$('#disp table td').eq(3).text(device.model);
-		$('#disp table td').eq(5).text(device.model);
-		$('#disp table td').eq(7).text(device.model);
-		$('#disp table td').eq(9).text(device.model);
-		$('#disp table td').eq(11).text(device.model);
-        document.addEventListener("pause",function(){
-			escribehistoria ('la app de pausó');
-		},false);//pause
-		document.addEventListener("resume",function(){
-		escribehistoria ('la app de reinició');	
-		},false);//resume
-		document.addEventListener("online",function(){
-		escribehistoria ('la app se conecto a la red');	
-		},false);//se conecto
-		document.addEventListener("offline",function(){
-		escribehistoria ('la app se desconecto de la red');	
-		},false);//se desconecto
+		$('#bepp').tap(function(){
+		navigator.notificacion.bepp(2);
+	});//tap beep
+		$('#bepp').tap(function(){
+		navigator.notificacion.bepp(1000);
+	});//tap vibrar
 		
-	},false);//ready device
-	
-});//document
+		},false);//deviceready
+});//ready
 
-function escribehistoria(accion){
-	$('#Historia').append('<li>'+accion+'</li>');
-}
+
